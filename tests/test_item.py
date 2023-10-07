dff2522643dfc0d624ef5d9c0c1fc2d8f0cf60a9
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-10-07 21:37:40 +0300

    tests for __repr__, __str__

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,14 +8,17 @@ item1 = Item("Смартфон", 10000, 20)
 item2 = Item("Ноутбук", 20000, 5)
 Item.pay_rate = 0.8
 
+
 def test_calculate_total_price():
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
+
 
 def test_apply_discount():
     item1.apply_discount()
     assert item1.price == 8000.0
     assert item2.price == 20000
+
 
 def test_name():
     item = Item('Телефон', 10000, 5)
@@ -25,11 +28,22 @@ def test_name():
     item.name = 'SuperSUPhone'
     assert item.name == 'SuperSUPho'
 
+
 def test_instantiate_from_csv():
     Item.instantiate_from_csv('src/items.csv')
     assert len(Item.all) == 5
+
 
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+
+def test___repr__():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+def test___str__():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
