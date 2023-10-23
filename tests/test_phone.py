352602341862d2525a289ddb6671a1cc6e647963
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-10-23 20:12:05 +0300

    test repr, init

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,6 +1,13 @@
 from src.phone import Phone
 
 phone1 = Phone("iPhone 14", 120_000, 5, 2)
+phone2 = Phone("iPhone 14", 120_000, 5, 0)
 
 def test___repr__():
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+def test___init__():
+    assert phone1.name == 'iPhone 14' #создание экземпляра
+    assert phone1.number_of_sim == 2 #количество симкарт > 0
+
+
