9b96d20d7bb33a0c7d12a9ccbdc5f66ee55b720d
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-10-23 20:11:59 +0300

    test repr, init

--- src/phone.py
+++ src/phone.py
@@ -7,5 +7,7 @@ class Phone(Item):
         if number_of_sim > 0:
             self.number_of_sim = number_of_sim
 
+
     def __repr__(self):
             return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
